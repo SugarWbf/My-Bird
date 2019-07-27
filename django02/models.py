8839747c79931c061f4764e7608cdd9a35e3e151
SugarStar <Sugar_wen@163.com>
2019-07-27 20:56:20 +0800

    添加boostrop文件，orm检查单常用操作

--- django02/models.py
+++ django02/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class student(models.Model):
     num = models.AutoField(primary_key=True),
     name = models.CharField(null=False, max_length=30)
-    #name = models.CharField(null=False, max_length=30)
+    name1 = models.CharField(null=False, max_length=30,default="")
     age = models.IntegerField()
 
     def __str__(self):
